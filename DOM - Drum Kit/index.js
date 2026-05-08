930e023dd4310cdc7d3f33e6e32456c6530d39e6
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-08 11:16:20 +0100

    Refactor drum button click handler to play audio based on button pressed

--- DOM - Drum Kit/index.js
+++ DOM - Drum Kit/index.js
@@ -2,12 +2,46 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    console.log((this.style.color = "white"));
+    let buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+      case "w":
+        let audio1 = new Audio("sounds/audio1.mp3");
+        audio1.play();
+        break;
+
+      case "a":
+        let audio2 = new Audio("sounds/audio2.mp3");
+        audio2.play();
+        break;
+
+      case "s":
+        let audio3 = new Audio("sounds/audio3.mp3");
+        audio3.play();
+        break;
+
+      case "d":
+        let audio4 = new Audio("sounds/audio4.mp3");
+        audio4.play();
+        break;
+
+      case "j":
+        let audio5 = new Audio("sounds/audio5.mp3");
+        audio5.play();
+        break;
+
+      case "k":
+        let audio6 = new Audio("sounds/audio6.mp3");
+        audio6.play();
+        break;
+
+      case "l":
+        let audio7 = new Audio("sounds/audio7.mp3");
+        audio7.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML);
+    }
   });
 }
-
-// let audio = new Audio("sound/audio1.mp3");
-// audio.play("click");
-
-// let audio = new Audio("sound/audio1.mp3");
-// audio.play("click");
