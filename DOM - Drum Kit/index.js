36863216ae6d6bd5758b0efe8dc4af4b930fbb1f
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-08 10:36:13 +0100

    Add: Implement event listeners for drum buttons to handle click events

--- DOM - Drum Kit/index.js
+++ DOM - Drum Kit/index.js
@@ -1,3 +1,9 @@
-document.querySelector("button").addEventListener("click", function () {
-  alert("I got clicked!");
-});
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+  function handleClick() {
+    alert("I got clicked!");
+  }
+}
