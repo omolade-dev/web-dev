6b51ea3c2b688853b4d28c73e615d31a539a15fe
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-08 10:53:22 +0100

    Update drum button click handler to alert on click and change text color

--- DOM - Drum Kit/index.js
+++ DOM - Drum Kit/index.js
@@ -1,9 +1,13 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-
-  function handleClick() {
-    alert("I got clicked!");
-  }
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    console.log((this.style.color = "white"));
+  });
 }
+
+// let audio = new Audio("sound/audio1.mp3");
+// audio.play("click");
+
+// let audio = new Audio("sound/audio1.mp3");
+// audio.play("click");
