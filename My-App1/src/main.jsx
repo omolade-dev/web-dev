af3f9798439427fb05cedd279542a6a2e43e1bae
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-12 16:13:33 +0100

    Refactor: Update index.html title and remove unused App component; modify main.jsx to render a simple food list

--- My-App1/src/main.jsx
+++ My-App1/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <div>
+    <h1>Food To Eat</h1>
+
+    <ul>
+      <li>Noodles</li>
+      <li>Rice</li>
+      <li>Beans</li>
+    </ul>
+  </div>,
+);
