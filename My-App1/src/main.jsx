567e00a56eb732a2c9640b35fb58e7fbfb3defae
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-13 15:19:18 +0100

    Refactor: Remove unused assets and simplify CSS; update main.jsx to display images and a food list

--- My-App1/src/main.jsx
+++ My-App1/src/main.jsx
@@ -1,12 +1,31 @@
 import { createRoot } from "react-dom/client";
+import "./index.css";
+import img from "./assets/IMG_8098.JPG";
+import img2 from "./assets/IMG_8099.JPG";
+import img3 from "./assets/IMG_8100.JPG";
 
-const name = "Omolade Ajala";
-const date = new Date();
-const currentYear = date.getFullYear();
+const customStyle = {
+  color: "green",
+  fontSize: "20px",
+  border: "1px solid black",
+};
 
 createRoot(document.getElementById("root")).render(
   <div>
-    <p>Created by {name}</p>
-    <p>Copyright {currentYear}</p>
+    <h1 style={customStyle} className="heading">
+      Favourite Food
+    </h1>
+
+    <ul>
+      <li>Noodles</li>
+      <li>Bacon</li>
+      <li>Jamon</li>
+    </ul>
+
+    <div>
+      <img src={img + "?grayscale"} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
+    </div>
   </div>,
 );
