93fce46b450793720a568eb363803daf4884a55d
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-13 15:26:01 +0100

    Refactor: Create List component to replace inline food list in main.jsx

---
+++ My-App1/src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Noodles</li>
+      <li>Bacon</li>
+      <li>Jamon</li>
+    </ul>
+  );
+}
+
+export default List;
--- My-App1/src/main.jsx
+++ My-App1/src/main.jsx
@@ -4,15 +4,12 @@ import img from "./assets/IMG_8098.JPG";
 import img2 from "./assets/IMG_8099.JPG";
 import img3 from "./assets/IMG_8100.JPG";
 import Heading from "./Heading";
+import List from "./List";
 
 createRoot(document.getElementById("root")).render(
   <div>
     <Heading />
-    <ul>
-      <li>Noodles</li>
-      <li>Bacon</li>
-      <li>Jamon</li>
-    </ul>
+    <List />
 
     <div>
       <img src={img + "?grayscale"} alt="" />
