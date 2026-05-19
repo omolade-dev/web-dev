0f4742f26af889246656c5f0e805c37157d4574f
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-19 10:11:04 +0100

    feat: add initial project setup with React and Vite
    - Added SVG assets for React and Vite logos.
    - Created main CSS file for styling.
    - Set up main entry point for the application in main.jsx.
    - Configured Vite with React plugin in vite.config.js.

--- Keeper/src/components/Note.jsx
+++ Keeper/src/components/Note.jsx
@@ -2,7 +2,7 @@ function Note() {
   return (
     <div>
       <div className="note">
-        <h1>This is a title</h1>
+        <h1>This is a title.</h1>
         <p>This is a note.</p>
       </div>
     </div>
