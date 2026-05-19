7e8bfafee7ca7ab1a55713b41260194eae1f98d2
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-19 15:33:31 +0100

    Refactor code structure for improved readability and maintainability

--- Contact-Prop/src/App.jsx
+++ Contact-Prop/src/App.jsx
@@ -1,20 +1,11 @@
-import pic17 from "./assets/pic17.jpeg";
+// import "./index.css";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img src={pic17} alt="" />
-        </div>
-
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Contact />
     </div>
   );
 }
--- Contact-Prop/src/components/Contact.jsx
+++ Contact-Prop/src/components/Contact.jsx
@@ -1,23 +1,26 @@
-const contacts = [
-  {
-    name: "Beyonce",
-    imgURL:
-      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-    phone: "+123 456 789",
-    email: "b@beyonce.com",
-  },
-  {
-    name: "Jack Bauer",
-    imgURL:
-      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
-    email: "jack@nowhere.com",
-  },
-  {
-    name: "Chuck Norris",
-    imgURL:
-      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-    phone: "+918 372 574",
-    email: "gmail@chucknorris.com",
-  },
-];
+import pic17 from "../assets/pic17.jpeg";
+import pic2 from "../assets/pic2.jpeg";
+import pic4 from "../assets/pic4.jpeg";
+
+function Contact() {
+  <div>
+    <h1>My Contacts</h1>
+
+    <h2>Beyonce</h2>
+    <img src={pic17} alt="avatar_img" />
+    <p>+123 456 789</p>
+    <p>b@beyonce.com</p>
+
+    <h2>Jack Bauer</h2>
+    <img src={pic2} alt="avatar_img" />
+    <p>+987 654 321</p>
+    <p>jack@nowhere.com</p>
+
+    <h2>Chuck Norris</h2>
+    <img src={pic4} alt="avatar_img" />
+    <p>+918 372 574</p>
+    <p>gmail@chucknorris.com</p>
+  </div>;
+}
+
+export default Contact;
