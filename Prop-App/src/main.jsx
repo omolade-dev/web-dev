f927946ed6885a273c350f2527584242ce1b65f4
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-19 14:07:58 +0100

    feat: implement Card component and update main rendering logic for contact display

--- Prop-App/src/main.jsx
+++ Prop-App/src/main.jsx
@@ -1,8 +1,40 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import pic13 from "./assets/pic13.jpeg";
+import pic20 from "./assets/pic20.jpeg";
+import pic21 from "./assets/pic21.jpeg";
+
+function Card(props) {
+  console.log(props);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <div>
-    <App />
+    <h1>My Contacts</h1>
+    <Card name="Beyonce" img={pic13} tel="+123 456 789" email="b@beyonce.com" />
+    <input id="fName" />
+
+    <Card
+      name="Jack Bauer"
+      img={pic20}
+      tel=">+987 654 321"
+      email="jack@nowhere.com"
+    />
+    <input id="fName" />
+
+    <Card
+      name="Chuck Norris"
+      img={pic21}
+      tel="+918 372 574"
+      email="gmail@chucknorris.com"
+    />
+    <input id="fName" />
   </div>,
 );
