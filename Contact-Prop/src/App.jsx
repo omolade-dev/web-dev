18f64f7847b188cf8fbcf1da6adf6132de7914b4
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-20 16:13:08 +0100

    feat: implement Card component and update App to use it for contact display

--- Contact-Prop/src/App.jsx
+++ Contact-Prop/src/App.jsx
@@ -1,11 +1,17 @@
-// import "./index.css";
-import Contact from "./components/Contact";
+import Card from "./components/Card";
+import pic17 from "./assets/pic17.jpeg";
+import "./index.css";
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <h1>My Contacts</h1>
-      <Contact />
+      <Card
+        name="Beyouce"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+        img={pic17}
+      />
     </div>
   );
 }
--- Contact-Prop/src/components/Contact.jsx
+++ Contact-Prop/src/components/Contact.jsx
@@ -1,26 +1,26 @@
-import pic17 from "../assets/pic17.jpeg";
-import pic2 from "../assets/pic2.jpeg";
-import pic4 from "../assets/pic4.jpeg";
+// import pic17 from "../assets/pic17.jpeg";
+// import pic2 from "../assets/pic2.jpeg";
+// import pic4 from "../assets/pic4.jpeg";
 
-function Contact() {
-  <div>
-    <h1>My Contacts</h1>
+// const contacts = [
+//   {
+//     name: "Beyonce",
+//     imgURL: { pic17 },
+//     phone: "+123 456 789",
+//     email: "b@beyonce.com",
+//   },
+//   {
+//     name: "Jack Bauer",
+//     imgURL: { pic2 },
+//     phone: "+987 654 321",
+//     email: "jack@nowhere.com",
+//   },
+//   {
+//     name: "Chuck Norris",
+//     imgURL: { pic4 },
+//     phone: "+918 372 574",
+//     email: "gmail@chucknorris.com",
+//   },
+// ];
 
-    <h2>Beyonce</h2>
-    <img src={pic17} alt="avatar_img" />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
-
-    <h2>Jack Bauer</h2>
-    <img src={pic2} alt="avatar_img" />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img src={pic4} alt="avatar_img" />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
-  </div>;
-}
-
-export default Contact;
+// export default contacts;
