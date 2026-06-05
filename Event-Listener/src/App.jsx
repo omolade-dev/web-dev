487172a32814eca8084160e4da3f1ece67244f19
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-06 00:24:07 +0100

    feat: add styling and layout to App component

--- Event-Listener/src/App.jsx
+++ Event-Listener/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
       <button>Submit</button>
