6f554f0b51f915505475b31d3417b0daef330b5a
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-06 00:30:49 +0100

    feat: add class names to input and button for styling

--- Event-Listener/src/App.jsx
+++ Event-Listener/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="container">
       <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
+      <input className="input" type="text" placeholder="What's your name?" />
       <button>Submit</button>
     </div>
   );
