a7b7ec51f1e2d31ff62b53736640230f781d2bca
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-06 01:04:19 +0100

    feat: implement state management and event handling in App component

--- Event-Listener/src/App.jsx
+++ Event-Listener/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+
+  function handleClick() {
+    console.log("Clicked");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{Hello}</h1>
       <input className="input" type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
