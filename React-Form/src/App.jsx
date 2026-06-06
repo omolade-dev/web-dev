a73dfb60bf4815e453c8ea3c9b161fca1a469617
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-06 03:41:24 +0100

    feat: add onChange handler to input for change event logging

--- React-Form/src/App.jsx
+++ React-Form/src/App.jsx
@@ -1,8 +1,17 @@
 function App() {
+  function handleChange() {
+    console.log("Changed");
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
-      <input className="input" type="text" placeholder="What's your name?" />
+      <input
+        onChange={handleChange}
+        className="input"
+        type="text"
+        placeholder="What's your name?"
+      />
       <button>Submit</button>
     </div>
   );
