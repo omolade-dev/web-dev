00750b42d85f2af698f437149a232657d1bdcd11
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-06 15:19:16 +0100

    feat: implement heading update and mouse event handling in App component

--- React-Form/src/App.jsx
+++ React-Form/src/App.jsx
@@ -2,6 +2,20 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
+  const [headingText, setHeading] = "";
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeading(name);
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
 
   function handleChange(event) {
     console.log(event.target.value);
@@ -10,14 +24,21 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {headingText}</h1>
       <input
         onChange={handleChange}
         className="input"
         type="text"
         placeholder="What's your name?"
       />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
