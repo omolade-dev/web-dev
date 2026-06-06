31725bffe7bac2aa4758caf86d031f814d2cd1c2
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-06 16:59:51 +0100

    feat: implement full name state management and update greeting display

--- Complex-state/src/App.jsx
+++ Complex-state/src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+
       <form action="">
-        <input name="fName" type="text" placeholder="First Name" />
-        <input name="lname" type="text" placeholder="Last Name" />
+        <input
+          name="fName"
+          type="text"
+          placeholder="First Name"
+          value={fName}
+          onChange={updateFName}
+        />
+
+        <input
+          name="lname"
+          type="text"
+          placeholder="Last Name"
+          value={lName}
+          onChange={updateLName}
+        />
+
         <button>Submit</button>
       </form>
     </div>
