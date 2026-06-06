85cde54757bba2f91c018137392c75c299ab26cc
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-06 16:42:58 +0100

    fix: correct placeholder text for first name input field

--- Complex-state/src/App.jsx
+++ Complex-state/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div className="container">
       <h1>Hello</h1>
       <form action="">
-        <input name="fName" type="text" placeholder="Last Name" />
+        <input name="fName" type="text" placeholder="First Name" />
         <input name="lname" type="text" placeholder="Last Name" />
         <button>Submit</button>
       </form>
